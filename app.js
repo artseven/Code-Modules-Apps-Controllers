0c8139f823504dcd68317496f414925e07fe02c5
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-10-03 22:30:12 -0400

    .toString same as console.log(function)

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var Person = function(firstname, lastname) {
+// var Person = function(firstname, lastname) {
 
-    this.firstname = firstname;
-    this.lastname = lastname;
-}
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+// }
 
-function logPerson(person) {
-    console.log(john);
-}
+// function logPerson(person) {
+//     console.log(john);
+// }
 
-var john = new Person('John', 'Doe');
+// var john = new Person('John', 'Doe');
 
-logPerson(john);
+// logPerson(john);
 
 
 // MODULE
@@ -19,9 +19,23 @@ var myApp = angular.module('myApp', []);
 // CONTROLLERS
 myApp.controller('mainController', function($scope) {
 
+    $scope.name = 'Jane Doe';
+    $scope.occupation = 'Coder';
+
+    $scope.getName = function() {
+        return 'John Doe';
+    }
+
     console.log($scope);//simply an object that is being injected in the view by controller
 
 });
 
 
+var searchPeople = function(firstname, lastname , height, age, occupation) {
+    return 'Jane Doe';
+}
+// same result as before
+var searchPeopleString = searchPeople.toString();
+
+console.log(searchPeople(1,2,3,4,5));
 
