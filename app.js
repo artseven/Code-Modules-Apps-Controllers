07c327a4135e11547f39a98c551c9983ba56edf3
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-10-03 22:11:20 -0400

    dependency injection(object passed to function)

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+var Person = function(firstname, lastname) {
+
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+
+
+function logPerson(person) {
+
+    console.log(john);
+}
+
+var john = new Person('John', 'Doe');
+logPerson(john);
+
+
 // MODULE
 var myApp = angular.module('myApp', []);
 
 // CONTROLLERS
 myApp.controller('mainController', function() {
-    
+
 });
 
 
